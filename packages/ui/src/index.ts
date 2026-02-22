1697ab09a247a12bc653486d7ae4123a68e1b721
itsaslamopenclawdata <itsaslamopenclawdata@gmail.com>
2026-02-22 10:37:45 +0000

    feat: add main index.ts with all component exports

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,85 @@
+// Core Components
+export { Button } from './components/Button';
+export type { ButtonProps } from './components/Button';
+
+export { Input } from './components/Input';
+export type { InputProps } from './components/Input';
+
+export { FormLabel } from './components/FormLabel';
+export type { FormLabelProps } from './components/FormLabel';
+
+export { FormField } from './components/FormField';
+export type { FormFieldProps } from './components/FormField';
+
+// Display Components
+export { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from './components/Card';
+export type { CardProps } from './components/Card';
+
+export { Badge } from './components/Badge';
+export type { BadgeProps } from './components/Badge';
+
+export { Alert } from './components/Alert';
+export type { AlertProps } from './components/Alert';
+
+// Photo Components
+export { PhotoCapture } from './components/PhotoCapture';
+export type { PhotoCaptureProps } from './components/PhotoCapture';
+
+export { ConfidenceCard } from './components/ConfidenceCard';
+export type { ConfidenceCardProps } from './components/ConfidenceCard';
+
+export { IdentificationResult } from './components/IdentificationResult';
+export type { IdentificationResultProps, Identification, BoundingBox } from './components/IdentificationResult';
+
+// Navigation & Layout
+export { Navbar } from './components/Navbar';
+export type { NavbarProps, NavItem } from './components/Navbar';
+
+export { Sidebar } from './components/Sidebar';
+export type { SidebarProps } from './components/Sidebar';
+
+export { Container } from './components/Container';
+export type { ContainerProps } from './components/Container';
+
+export { Grid } from './components/Grid';
+export type { GridProps } from './components/Grid';
+
+// Data Visualization
+export { ProgressBar } from './components/ProgressBar';
+export type { ProgressBarProps } from './components/ProgressBar';
+
+export { DonutChart } from './components/DonutChart';
+export type { DonutChartProps, ChartData } from './components/DonutChart';
+
+export { StatCard } from './components/StatCard';
+export type { StatCardProps } from './components/StatCard';
+
+// Map & Location
+export { LocationBadge } from './components/LocationBadge';
+export type { LocationBadgeProps } from './components/LocationBadge';
+
+export { MapPlaceholder } from './components/MapPlaceholder';
+export type { MapPlaceholderProps } from './components/MapPlaceholder';
+
+// Modal & Overlay
+export { Modal } from './components/Modal';
+export type { ModalProps } from './components/Modal';
+
+export { Dialog, DialogHeader, DialogTitle, DialogDescription, DialogContent, DialogFooter } from './components/Dialog';
+export type { DialogProps, DialogHeaderProps, DialogTitleProps, DialogDescriptionProps, DialogContentProps, DialogFooterProps } from './components/Dialog';
+
+export { Overlay } from './components/Overlay';
+export type { OverlayProps } from './components/Overlay';
+
+// State Components
+export { Loading } from './components/Loading';
+export type { LoadingProps } from './components/Loading';
+
+export { EmptyState } from './components/EmptyState';
+export type { EmptyStateProps } from './components/EmptyState';
+
+export { ErrorState } from './components/ErrorState';
+export type { ErrorStateProps } from './components/ErrorState';
+
+// Utilities
+export { cn } from './utils/cn';
